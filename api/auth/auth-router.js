7ea5bd6cba048cbe6f3d926163d33ae86f457997
Lee Damien <notesong.git@outlook.com>
2020-05-20 21:51:47 -0400

    Basic server setup

---
+++ api/auth/auth-router.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Users = require("../users/users-model");
+
+const router = express.Router();
+
+module.exports = router;
---
+++ api/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const restricted = require("../middleware/restricted.js");
+const usersRouter = require("./users/users-router.js");
+const authRouter = require("./auth/auth-router.js");
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use("/api/users", restricted, usersRouter);
+server.use("/api/auth", authRouter);
+
+server.get("/", (req, res) => {
+  res.json({ api: "up" });
+});
+
+server.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    message: "Something went wrong.",
+  });
+});
+
+module.exports = server;
