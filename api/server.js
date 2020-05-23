350efcb195806433d7791b5d04e50008de3a0f51
Lee Damien <notesong.git@outlook.com>
2020-05-23 15:24:24 -0400

    Remove restricted from users endpoint

--- api/server.js
+++ api/server.js
@@ -13,7 +13,7 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-server.use("/api/users", restricted, usersRouter);
+server.use("/api/users", usersRouter);
 server.use("/api/articles", restricted, articlesRouter);
 server.use("/api/auth", authRouter);
 
