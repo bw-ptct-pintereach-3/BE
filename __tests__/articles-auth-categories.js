735d2b0ad8f8a529e59c4bc8480e633e92c062d4
Lee Damien <notesong.git@outlook.com>
2020-05-29 22:35:22 -0400

    Tests for categories

--- __tests__/articles-auth-categories.js
+++ __tests__/articles-auth-categories.js
@@ -4,12 +4,12 @@ const db = require("../data/dbconfig");
 require("dotenv").config();
 
 beforeEach(async () => {
-  await db.seed.run()
-})
+  await db.seed.run();
+});
 
 afterAll(async () => {
-  await db.destroy()
-})
+  await db.destroy();
+});
 
 const token = process.env.TOKEN_1;
 
@@ -25,33 +25,32 @@ const token2 = process.env.TOKEN_2;
 
 describe("articles integration test", () => {
   const data = {
-    "category_id": "1",
-    "url": "http://hi.com",
+    category_id: "1",
+    url: "http://hi.com",
   };
 
   const data_no_category_id = {
-    "url": "http://hi.com",
+    url: "http://hi.com",
   };
 
   const data_no_url = {
-    "category_id": "1",
+    category_id: "1",
   };
 
   const data_category_id_high = {
-    "category_id": "11",
-    "url": "http://hi.com",
+    category_id: "11",
+    url: "http://hi.com",
   };
 
   const data_category_id_low = {
-    "category_id": "0",
-    "url": "http://hi.com",
+    category_id: "0",
+    url: "http://hi.com",
   };
 
   const data_url_empty_string = {
-    "category_id": "1",
-    "url": "",
+    category_id: "1",
+    url: "",
   };
-
 
   /////////////////////////////////
   // Gets
@@ -60,33 +59,32 @@ describe("articles integration test", () => {
   it("GET /articles", async () => {
     const res = await supertest(server)
       .get("/api/articles")
-      .set('Authorization', token);
+      .set("Authorization", token);
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(200);
-    expect(res.body[0].url).toBe("https://google.com")
-    expect(res.body[1].url).toBe("https://twitter.com")
+    expect(res.body[0].url).toBe("https://google.com");
+    expect(res.body[1].url).toBe("https://twitter.com");
   });
 
   // correct values
   it("GET /articles/:id", async () => {
     const res = await supertest(server)
       .get("/api/articles/1")
-      .set('Authorization', token);
+      .set("Authorization", token);
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(200);
-    expect(res.body.url).toBe("https://google.com")
+    expect(res.body.url).toBe("https://google.com");
   });
 
   // invalid article id
   it("GET /articles/:id", async () => {
     const res = await supertest(server)
       .get("/api/articles/30")
-      .set('Authorization', token);
+      .set("Authorization", token);
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(404);
-    expect(res.body.error).toBe("The article id was not found.")
+    expect(res.body.error).toBe("The article id was not found.");
   });
-
 
   ////////////////////////////////
   // Posts
@@ -96,21 +94,21 @@ describe("articles integration test", () => {
     const res = await supertest(server)
       .post("/api/articles")
       .send(data)
-      .set('Authorization', token);
+      .set("Authorization", token);
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(201);
-    expect(res.body.url).toBe("http://hi.com")
-    expect(res.body.user_id).toBe(1)
+    expect(res.body.url).toBe("http://hi.com");
+    expect(res.body.user_id).toBe(1);
   });
 
   // no data
   it("POST /articles", async () => {
     const res = await supertest(server)
       .post("/api/articles")
-      .set('Authorization', token);
+      .set("Authorization", token);
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(400);
-    expect(res.body.error).toBe("Request missing url and category.")
+    expect(res.body.error).toBe("Request missing url and category.");
   });
 
   // no url
@@ -118,10 +116,10 @@ describe("articles integration test", () => {
     const res = await supertest(server)
       .post("/api/articles")
       .send(data_no_url)
-      .set('Authorization', token);
+      .set("Authorization", token);
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(400);
-    expect(res.body.error).toBe("Request missing required field: url")
+    expect(res.body.error).toBe("Request missing required field: url");
   });
 
   // url empty string
@@ -129,10 +127,10 @@ describe("articles integration test", () => {
     const res = await supertest(server)
       .post("/api/articles")
       .send(data_url_empty_string)
-      .set('Authorization', token);
+      .set("Authorization", token);
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(400);
-    expect(res.body.error).toBe("The url cannot be an empty string.")
+    expect(res.body.error).toBe("The url cannot be an empty string.");
   });
 
   // no category_id
@@ -140,10 +138,10 @@ describe("articles integration test", () => {
     const res = await supertest(server)
       .post("/api/articles")
       .send(data_no_category_id)
-      .set('Authorization', token);
+      .set("Authorization", token);
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(400);
-    expect(res.body.error).toBe("Request missing required field: category_id")
+    expect(res.body.error).toBe("Request missing required field: category_id");
   });
 
   // invalid category_id range - too low
@@ -151,10 +149,12 @@ describe("articles integration test", () => {
     const res = await supertest(server)
       .post("/api/articles")
       .send(data_category_id_low)
-      .set('Authorization', token);
+      .set("Authorization", token);
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(400);
-    expect(res.body.error).toBe("The category_id must be in the range of 1 to 10.")
+    expect(res.body.error).toBe(
+      "The category_id must be in the range of 1 to 10."
+    );
   });
 
   // invalid category_id range - too high
@@ -162,12 +162,13 @@ describe("articles integration test", () => {
     const res = await supertest(server)
       .post("/api/articles")
       .send(data_category_id_high)
-      .set('Authorization', token);
+      .set("Authorization", token);
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(400);
-    expect(res.body.error).toBe("The category_id must be in the range of 1 to 10.")
+    expect(res.body.error).toBe(
+      "The category_id must be in the range of 1 to 10."
+    );
   });
-
 
   ////////////////////////////////
   // Puts
@@ -177,11 +178,11 @@ describe("articles integration test", () => {
     const res = await supertest(server)
       .put("/api/articles/1")
       .send(data)
-      .set('Authorization', token);
+      .set("Authorization", token);
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(200);
-    expect(res.body.url).toBe("http://hi.com")
-    expect(res.body.user_id).toBe(1)
+    expect(res.body.url).toBe("http://hi.com");
+    expect(res.body.user_id).toBe(1);
   });
 
   // article belongs to another user
@@ -189,10 +190,12 @@ describe("articles integration test", () => {
     const res = await supertest(server)
       .put("/api/articles/1")
       .send(data)
-      .set('Authorization', token2);
+      .set("Authorization", token2);
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(401);
-    expect(res.body.error).toBe("The article id is not associated with this user.")
+    expect(res.body.error).toBe(
+      "The article id is not associated with this user."
+    );
   });
 
   // invalid article id
@@ -200,22 +203,21 @@ describe("articles integration test", () => {
     const res = await supertest(server)
       .put("/api/articles/30")
       .send(data)
-      .set('Authorization', token);
+      .set("Authorization", token);
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(404);
-    expect(res.body.error).toBe("The article id was not found.")
+    expect(res.body.error).toBe("The article id was not found.");
   });
 
   // no data
   it("PUT /articles/1", async () => {
     const res = await supertest(server)
       .put("/api/articles/1")
-      .set('Authorization', token);
+      .set("Authorization", token);
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(400);
-    expect(res.body.error).toBe("Request missing url and category.")
+    expect(res.body.error).toBe("Request missing url and category.");
   });
-
 
   ////////////////////////////////
   // Deletes
@@ -224,20 +226,20 @@ describe("articles integration test", () => {
   it("DELETE /articles/1", async () => {
     const res = await supertest(server)
       .delete("/api/articles/1")
-      .set('Authorization', token);
+      .set("Authorization", token);
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(200);
-    expect(res.body.message).toBe("The article has been deleted.")
+    expect(res.body.message).toBe("The article has been deleted.");
   });
 
   // invalid article id
   it("DELETE /articles/30", async () => {
     const res = await supertest(server)
       .delete("/api/articles/30")
-      .set('Authorization', token);
+      .set("Authorization", token);
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(404);
-    expect(res.body.error).toBe("The article id was not found.")
+    expect(res.body.error).toBe("The article id was not found.");
   });
 });
 
@@ -251,31 +253,31 @@ describe("articles integration test", () => {
 
 describe("auth integration test", () => {
   const data_register = {
-    "username": "notesong",
-    "password": "pw",
+    username: "notesong",
+    password: "pw",
   };
 
   const data_login = {
-    "username": "note",
-    "password": "pw",
+    username: "note",
+    password: "pw",
   };
 
   const data_no_username = {
-    "password": "pw",
+    password: "pw",
   };
 
   const data_no_password = {
-    "username": "note",
+    username: "note",
   };
 
   const data_username_empty_string = {
-    "username": "",
-    "password": "pw",
+    username: "",
+    password: "pw",
   };
 
   const data_password_empty_string = {
-    "username": "note",
-    "password": "",
+    username: "note",
+    password: "",
   };
 
   /////////////////////////////////
@@ -288,7 +290,7 @@ describe("auth integration test", () => {
       .send(data_register);
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(201);
-    expect(res.body.username).toBe("notesong")
+    expect(res.body.username).toBe("notesong");
   });
 
   // duplicate username
@@ -298,16 +300,15 @@ describe("auth integration test", () => {
       .send(data_login);
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(401);
-    expect(res.body.error).toBe("Username is already taken.")
+    expect(res.body.error).toBe("Username is already taken.");
   });
 
   // no data
   it("POST /auth/register", async () => {
-    const res = await supertest(server)
-      .post("/api/auth/register")
+    const res = await supertest(server).post("/api/auth/register");
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(400);
-    expect(res.body.error).toBe("Request missing username and password.")
+    expect(res.body.error).toBe("Request missing username and password.");
   });
 
   // no username
@@ -317,7 +318,7 @@ describe("auth integration test", () => {
       .send(data_no_username);
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(400);
-    expect(res.body.error).toBe("Request missing required field: username.")
+    expect(res.body.error).toBe("Request missing required field: username.");
   });
 
   // no password
@@ -327,7 +328,7 @@ describe("auth integration test", () => {
       .send(data_no_password);
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(400);
-    expect(res.body.error).toBe("Request missing required field: password.")
+    expect(res.body.error).toBe("Request missing required field: password.");
   });
 
   // empty username
@@ -337,7 +338,7 @@ describe("auth integration test", () => {
       .send(data_username_empty_string);
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(400);
-    expect(res.body.error).toBe("A username cannot be an empty string.")
+    expect(res.body.error).toBe("A username cannot be an empty string.");
   });
 
   // empty password
@@ -347,7 +348,7 @@ describe("auth integration test", () => {
       .send(data_password_empty_string);
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(400);
-    expect(res.body.error).toBe("A password cannot be an empty string.")
+    expect(res.body.error).toBe("A password cannot be an empty string.");
   });
 
   /////////////////////////////////
@@ -360,15 +361,63 @@ describe("auth integration test", () => {
       .send(data_login);
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(200);
-    expect(res.body.username).toContain("note")
+    expect(res.body.username).toContain("note");
   });
 
   // no data
   it("POST /auth/login", async () => {
-    const res = await supertest(server)
-      .post("/api/auth/login")
+    const res = await supertest(server).post("/api/auth/login");
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(400);
-    expect(res.body.error).toContain("Request missing username and password.")
+    expect(res.body.error).toContain("Request missing username and password.");
+  });
+});
+
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+//
+// Categories
+//
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+
+describe("categories integration test", () => {
+  it("GET /categories", async () => {
+    const res = await supertest(server)
+      .get("/api/categories")
+      .set("Authorization", token);
+    expect(res.statusCode).toBe(200);
+    expect(res.type).toBe("application/json");
+    expect(res.body).toHaveLength(10);
+    expect(res.body[0].category).toBe("Education");
+    expect(res.body[1].category).toBe("Recreation");
+  });
+  it("GET /categories/1", async () => {
+    const res = await supertest(server)
+      .get("/api/categories/1")
+      .set("Authorization", token);
+    expect(res.statusCode).toBe(200);
+    expect(res.type).toBe("application/json");
+    expect(res.body.category).toBe("Education");
+  });
+  it("GET /categories/0", async () => {
+    const res = await supertest(server)
+      .get("/api/categories/0")
+      .set("Authorization", token);
+    expect(res.statusCode).toBe(400);
+    expect(res.type).toBe("application/json");
+    expect(res.body.error).toBe(
+      "The category_id must be in the range of 1 to 10."
+    );
+  });
+  it("GET /categories/11", async () => {
+    const res = await supertest(server)
+      .get("/api/categories/11")
+      .set("Authorization", token);
+    expect(res.statusCode).toBe(400);
+    expect(res.type).toBe("application/json");
+    expect(res.body.error).toBe(
+      "The category_id must be in the range of 1 to 10."
+    );
   });
 });
